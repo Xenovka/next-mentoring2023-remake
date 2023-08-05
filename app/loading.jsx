267711a6d1c3ca4cbafcb276a0f4d2aa305b3a58
Xenovka <wendyanto18@gmail.com>
2023-08-05 09:09:04 +0700

    change loading text

--- app/loading.jsx
+++ app/loading.jsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Loading() {
   return (
     <div className="flex flex-col justify-center items-center h-screen w-screen">
-      <Image src="/assets/zachery/zach-wave.gif" alt="Zach Loading Screen" height={426} width={426} />
-      <h3 className="text-[40px] text-center font-bold italic text-white -mt-7">Now Loading ...</h3>
+      <Image src="/assets/zachery/zach-wave.gif" alt="Zach Loading Screen" height={426} width={426} priority />
+      <h3 className="text-[40px] text-center font-bold italic text-white -mt-7">Please Wait ...</h3>
       <p className="text-center text-white font-bold italic">Pro-Tip: A journey is about to begin. Prepare yourself!</p>
     </div>
   );
