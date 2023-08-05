eff01c2c966e73229bd745165e85227bc475ab47
Xenovka <wendyanto18@gmail.com>
2023-08-05 18:54:48 +0700

    fix typo and function position

--- app/divisi/page.jsx
+++ app/divisi/page.jsx
@@ -1,21 +1,21 @@
 import Footer from "@/components/footer";
 import DivisionContent from "@/app/divisi/_components/division-content";
 import DivisionSwiper from "@/app/divisi/_components/division-swiper";
-import DivisionHeader from "@/app/divisi/components/division-header";
+import DivisionHeader from "@/app/divisi/_components/division-header";
 
 import { DivisionContextProvider } from "@/app/divisi/_context/division-context";
 
-async function getData() {
-  const response = await fetch(process.env.NEXT_PUBLIC_API_URL);
-  const body = response.json();
-
-  return body;
-}
-
-// Fetch data from API asycronously
-const divisionData = await getData();
-
 export default async function Divisi() {
+  async function getData() {
+    const response = await fetch(process.env.NEXT_PUBLIC_API_URL);
+    const body = response.json();
+
+    return body;
+  }
+
+  // Fetch data from API asycronously
+  const divisionData = await getData();
+
   return (
     <>
       {/* Wrap division with context provider so the the children components can access the context value */}
