338c6b8890666fc762d74819f8e45cab0e01a9af
Xenovka <wendyanto18@gmail.com>
2023-08-08 15:20:08 +0700

    adding flex-col

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="w-full mt-4 h-auto pb-2">
+    <footer className="w-full mt-4 h-auto pb-2 flex flex-col justify-center items-center">
       <p className="text-center text-3xl text-[#9BCDEC] font-bold italic drop-shadow-[6px_6px_4px_#000000]">
         #PerceivePursuePersevere
       </p>
