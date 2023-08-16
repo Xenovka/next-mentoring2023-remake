8b7b06612164a1e091f47eece2dac574693a4dd4
Fernando Khorasani <fernando.khorasani@gmail.com>
2023-08-16 15:14:13 +0700

    update-button-ui

--- app/badge/_components/FormData.jsx
+++ app/badge/_components/FormData.jsx
@@ -3,152 +3,177 @@ import { useState, useEffect } from "react";
 import styles from "@/public/styles/about.module.css";
 
 export default function FormData({ setStage, stage }) {
-  const [name, setName] = useState("")
-  const [gender, setGender] = useState("")
-  const [nim, setNim] = useState("")
-  const [jurusan, setJurusan] = useState("")
-  const [group, setGroup] = useState("")
-  const [mentor, setMentor] = useState("")
+  const [name, setName] = useState("");
+  const [gender, setGender] = useState("");
+  const [nim, setNim] = useState("");
+  const [jurusan, setJurusan] = useState("");
+  const [group, setGroup] = useState("");
+  const [mentor, setMentor] = useState("");
 
   useEffect(() => {
-    const name = localStorage.getItem("name")
-    const gender = localStorage.getItem("gender")
-    const nim = localStorage.getItem("nim")
-    const jurusan = localStorage.getItem("jurusan")
-    const group = localStorage.getItem("group")
-    const mentor = localStorage.getItem("mentor")
-    if (name) setName(name)
-    if (gender) setGender(gender)
-    if (nim) setNim(nim)
-    if (jurusan) setJurusan(jurusan)
-    if (group) setGroup(group)
-    if (mentor) setMentor(mentor)
-  }, [])
+    const name = localStorage.getItem("name");
+    const gender = localStorage.getItem("gender");
+    const nim = localStorage.getItem("nim");
+    const jurusan = localStorage.getItem("jurusan");
+    const group = localStorage.getItem("group");
+    const mentor = localStorage.getItem("mentor");
+    if (name) setName(name);
+    if (gender) setGender(gender);
+    if (nim) setNim(nim);
+    if (jurusan) setJurusan(jurusan);
+    if (group) setGroup(group);
+    if (mentor) setMentor(mentor);
+  }, []);
 
   function handleDataSubmit() {
-    localStorage.setItem('name', name)
-    localStorage.setItem('gender', gender)
-    localStorage.setItem('nim', nim)
-    localStorage.setItem('jurusan', jurusan)
-    localStorage.setItem('group', group)
-    localStorage.setItem('mentor', mentor)
-    setStage(stage + 1)
+    localStorage.setItem("name", name);
+    localStorage.setItem("gender", gender);
+    localStorage.setItem("nim", nim);
+    localStorage.setItem("jurusan", jurusan);
+    localStorage.setItem("group", group);
+    localStorage.setItem("mentor", mentor);
+    setStage(stage + 1);
   }
 
   return (
-    <div className="flex flex-col gap-10 mx-40 justify-center items-center">
-      <h1 className="text-3xl font-extrabold text-center uppercase">
-        Welcome! Please Insert Your Data!
-      </h1>
-      <div className="flex flex-col gap-5">
-        <div className="flex justify-between">
-          <p className="text-lg font-semibold">Nama</p>
-          <div className="flex gap-10">
-            <p>:</p>
-            <input
-              type="text"
-              placeholder="Alexandria Dinda"
-              className=" w-80 px-2 bg-[#0F1633]"
-              value={name}
-              name={"name"}
-              onChange={(e) => setName(e.target.value)}
-              required
-            />
-          </div>
-        </div>
-        <div className="flex justify-between">
-          <p className="text-lg font-semibold">Jenis Kelamin</p>
-          <div className="flex gap-12 mr-[54px]">
-            <p>:</p>
-            <input type="radio" name="gender" value='male' required checked={gender === "male"}
-              onChange={e => setGender(e.target.value)} /> Male
-            <input type="radio" name="gender" value="female" required checked={gender === "female"}
-              onChange={e => setGender(e.target.value)} /> Female
-          </div>
-        </div>
-        <div className="flex justify-between">
-          <p className="text-lg font-semibold">NIM</p>
-          <div className="flex gap-10">
-            <p>:</p>
-            <input
-              type="text"
-              placeholder="00000072345"
-              className=" w-80 px-2 bg-[#0F1633]"
-              onChange={(e) => setNim(e.target.value)}
-              value={nim}
-              name={"nim"}
-              required
-            />
-          </div>
-        </div>
-        <div className="flex justify-between">
-          <p className="text-lg font-semibold">Jurusan</p>
-          <div className="flex gap-10">
-            <p>:</p>
-            <input
-              type="text"
-              placeholder="Desain Komunikasi Visual"
-              className=" w-80 px-2 bg-[#0F1633]"
-              onChange={(e) => setJurusan(e.target.value)}
-              value={jurusan}
-              name={"jurusan"}
-              required
-            />
-          </div>
-        </div>
-        <div className="flex justify-between">
-          <p className="text-lg font-semibold">No. Kelompok</p>
-          <div className="flex gap-10">
-            <p>:</p>
-            <input
-              type="number"
-              min="1"
-              placeholder="75"
-              className=" w-80 px-2 bg-[#0F1633]"
-              onChange={(e) => setGroup(e.target.value)}
-              value={group}
-              name={"group"}
-              required
-            />
-          </div>
-        </div>
-        <div className="flex justify-between">
-          <p className="text-lg font-semibold">Nama Mentor</p>
-          <div className="flex gap-10">
-            <p>:</p>
-            <input
-              type="text"
-              placeholder="Syaiful Anam"
-              className=" w-80 px-2 bg-[#0F1633]"
-              onChange={(e) => setMentor(e.target.value)}
-              value={mentor}
-              name={"mentor"}
-              required
-            />
-          </div>
-        </div>
-      </div>
-      <p className="text-center opacity-50 italic">
-        Diharapkan kepada mentee untuk mengisi data diri dengan benar!
-      </p>
-      <div className="flex justify-evenly w-full">
+    <>
       <button
         className={`${styles.gradienbackmodal} w-40 h-10 text-base font-semibold flex justify-center items-center`}
         type="submit"
         onClick={() => {
-          setStage(1)
+          setStage(1);
         }}
       >
         Back
       </button>
-      <button
-        className={`${styles.gradienbackmodal} w-40 h-10 text-base font-semibold flex justify-center items-center`}
-        type="submit"
-        onClick={handleDataSubmit}
-      >
-        Continue
-      </button>
+      <div className="flex flex-col gap-10 mx-40 justify-center items-center">
+        <h1 className="text-3xl font-extrabold text-center uppercase">
+          Welcome! Please Insert Your Data!
+        </h1>
+        <div className="flex flex-col gap-5">
+          <div className="flex justify-between">
+            <p className="text-lg font-semibold">Nama</p>
+            <div className="flex gap-10">
+              <p>:</p>
+              <input
+                type="text"
+                placeholder="Alexandria Dinda"
+                className=" w-80 px-2 bg-[#0F1633]"
+                value={name}
+                name={"name"}
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
+            </div>
+          </div>
+          <div className="flex justify-between">
+            <p className="text-lg font-semibold">Jenis Kelamin</p>
+            <div className="flex gap-12 mr-[54px]">
+              <p>:</p>
+              <input
+                type="radio"
+                name="gender"
+                value="male"
+                required
+                checked={gender === "male"}
+                onChange={(e) => setGender(e.target.value)}
+              />{" "}
+              Male
+              <input
+                type="radio"
+                name="gender"
+                value="female"
+                required
+                checked={gender === "female"}
+                onChange={(e) => setGender(e.target.value)}
+              />{" "}
+              Female
+            </div>
+          </div>
+          <div className="flex justify-between">
+            <p className="text-lg font-semibold">NIM</p>
+            <div className="flex gap-10">
+              <p>:</p>
+              <input
+                type="text"
+                placeholder="00000072345"
+                className=" w-80 px-2 bg-[#0F1633]"
+                onChange={(e) => setNim(e.target.value)}
+                value={nim}
+                name={"nim"}
+                required
+              />
+            </div>
+          </div>
+          <div className="flex justify-between">
+            <p className="text-lg font-semibold">Jurusan</p>
+            <div className="flex gap-10">
+              <p>:</p>
+              <input
+                type="text"
+                placeholder="Desain Komunikasi Visual"
+                className=" w-80 px-2 bg-[#0F1633]"
+                onChange={(e) => setJurusan(e.target.value)}
+                value={jurusan}
+                name={"jurusan"}
+                required
+              />
+            </div>
+          </div>
+          <div className="flex justify-between">
+            <p className="text-lg font-semibold">No. Kelompok</p>
+            <div className="flex gap-10">
+              <p>:</p>
+              <input
+                type="number"
+                min="1"
+                placeholder="75"
+                className=" w-80 px-2 bg-[#0F1633]"
+                onChange={(e) => setGroup(e.target.value)}
+                value={group}
+                name={"group"}
+                required
+              />
+            </div>
+          </div>
+          <div className="flex justify-between">
+            <p className="text-lg font-semibold">Nama Mentor</p>
+            <div className="flex gap-10">
+              <p>:</p>
+              <input
+                type="text"
+                placeholder="Syaiful Anam"
+                className=" w-80 px-2 bg-[#0F1633]"
+                onChange={(e) => setMentor(e.target.value)}
+                value={mentor}
+                name={"mentor"}
+                required
+              />
+            </div>
+          </div>
+        </div>
+        <p className="text-center opacity-50 italic">
+          Diharapkan kepada mentee untuk mengisi data diri dengan benar!
+        </p>
+        <div className="flex justify-evenly w-full">
+          <button
+            className={`${styles.gradienbackmodal} w-40 h-10 text-base font-semibold flex justify-center items-center`}
+            type="submit"
+            onClick={() => {
+              setStage(1);
+            }}
+          >
+            Back
+          </button>
+          <button
+            className={`${styles.gradienbackmodal} w-40 h-10 text-base font-semibold flex justify-center items-center`}
+            type="submit"
+            onClick={handleDataSubmit}
+          >
+            Continue
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
--- app/badge/_components/Notif.jsx
+++ app/badge/_components/Notif.jsx
@@ -1,43 +1,44 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import styles from "@/public/styles/about.module.css";
 
-export default function Notif({ setStage, stage, setShowModal}) {
-
+export default function Notif({ setStage, stage, setShowModal }) {
   return (
-    <div className="flex flex-col gap-10 mx-40 justify-center items-center">
-      <h1 className="text-3xl font-extrabold text-center uppercase">
-        Notification?
-      </h1>
-      <p className="text-justify">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut, eius
-        labore doloribus beatae ab in delectus id eveniet odit quae suscipit,
-        adipisci maxime perspiciatis animi? Id, ipsam. Vitae, quia voluptates!
-        Cupiditate aspernatur eius reiciendis, quae deserunt, aperiam voluptas
-        architecto, inventore commodi ipsum at omnis. Id aliquid, facere cumque
-        tenetur similique non reprehenderit cum optio veritatis rem neque
-        repellendus pariatur quasi beatae minus dignissimos sed eos sint! Eius
-        expedita harum molestiae, aliquam quas odit pariatur doloribus explicabo
-        delectus quidem quaerat nisi unde sunt, voluptatum ratione sed maxime
-        eveniet ullam iusto. Alias, nihil consequuntur! Aperiam doloremque
-        fugiat aliquam sequi mollitia culpa cumque?
-      </p>
+    <>
       <button
-        className={`${styles.gradienbackmodal} w-60 h-10 text-base font-semibold flex justify-center items-center`}
+        className={`w-60 h-20 text-3xl font-semibold flex justify-center items-center absolute right-[15vw] top-[15vh]`}
         onClick={() => {
-          setStage(2)
-        }}
-      >
-        I'M READY!
-      </button>
-      <button
-        className={`w-60 h-10 text-base font-semibold flex justify-center items-center`}
-        onClick={() => {
-          setShowModal(false)
-          setStage(1)
+          setShowModal(false);
+          setStage(1);
         }}
       >
         Back
       </button>
-    </div>
+      <div className="flex flex-col gap-10 mx-40 justify-center items-center relative">
+        <h1 className="text-3xl font-extrabold text-center uppercase">
+          Notification?
+        </h1>
+        <p className="text-justify">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut, eius
+          labore doloribus beatae ab in delectus id eveniet odit quae suscipit,
+          adipisci maxime perspiciatis animi? Id, ipsam. Vitae, quia voluptates!
+          Cupiditate aspernatur eius reiciendis, quae deserunt, aperiam voluptas
+          architecto, inventore commodi ipsum at omnis. Id aliquid, facere
+          cumque tenetur similique non reprehenderit cum optio veritatis rem
+          neque repellendus pariatur quasi beatae minus dignissimos sed eos
+          sint! Eius expedita harum molestiae, aliquam quas odit pariatur
+          doloribus explicabo delectus quidem quaerat nisi unde sunt, voluptatum
+          ratione sed maxime eveniet ullam iusto. Alias, nihil consequuntur!
+          Aperiam doloremque fugiat aliquam sequi mollitia culpa cumque?
+        </p>
+        <button
+          className={`${styles.gradienbackmodal} w-60 h-10 text-base font-semibold flex justify-center items-center`}
+          onClick={() => {
+            setStage(2);
+          }}
+        >
+          I'M READY!
+        </button>
+      </div>
+    </>
   );
 }
