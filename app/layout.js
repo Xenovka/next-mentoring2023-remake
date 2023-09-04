6234c255384283d3c73565f902bea7a01366c58c
Fernando Khorasani <fernando.khorasani@gmail.com>
2023-09-04 14:03:03 +0700

    update meta description

--- app/layout.js
+++ app/layout.js
@@ -5,13 +5,15 @@ const karla = Karla({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Mentoring UMN 2023",
-  description: "Official Website for Career Building UMN 2023 (Mentoring)."
+  description: "Official Website for Character Building UMN 2023 (Mentoring).",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${karla.className} hidescroll text-white`}>{children}</body>
+      <body className={`${karla.className} hidescroll text-white`}>
+        {children}
+      </body>
     </html>
   );
 }
